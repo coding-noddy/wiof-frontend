b3681247c17f1e5df9f0c939ea4035aea796a961
Naved Anwar Maulavi <nmaulavi5@gmail.com>
2025-09-01 10:52:10 +0530

    refactor: improve config service typing

--- src/app/config.service.ts
+++ src/app/config.service.ts
@@ -1,37 +1,29 @@
 import { Injectable } from '@angular/core';
-import {
-  AngularFirestore,
-  AngularFirestoreCollection
-} from '@angular/fire/compat/firestore';
-import { from, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { FIREBASE_COLLECTION } from './app.constants';
+
+export interface Config {
+  file: string;
+  [key: string]: any;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
-  congifCollection: AngularFirestoreCollection<any>;
+  constructor(private database: AngularFirestore) {}
 
-  constructor(public database: AngularFirestore) {
-    this.congifCollection = this.database.collection(
-      FIREBASE_COLLECTION.CONFIG
-    );
-  }
-
-  getConfig(file: string): Observable<any> {
-    const configCollectionByCategory = this.database.collection(
-      FIREBASE_COLLECTION.CONFIG,
-      (ref) => ref.where('file', '==', file)
-    );
-    return configCollectionByCategory.snapshotChanges().pipe(
-      map((actions) =>
-        actions.map((a) => {
-          const data = a.payload.doc.data();
-          // data.id = a.payload.doc.id;
-          return data;
+  getConfig(file: string): Observable<Config[]> {
+    return this.database
+      .collection<Config>(FIREBASE_COLLECTION.CONFIG, ref => ref.where('file', '==', file))
+      .valueChanges()
+      .pipe(
+        catchError((error) => {
+          console.error('Error loading config', error);
+          return of([]);
         })
-      )
-    );
+      );
   }
 }
