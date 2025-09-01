3b2295432229cdcfaab43fcf1dc4a82a68604043
Naved Anwar Maulavi <nmaulavi5@gmail.com>
2025-09-01 10:00:54 +0530

    chore: add material theme stylesheet

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,3 +1,4 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 import { CoffeeConversationService } from 'src/app/services/coffee-conversation.service';
 import { NgoInFocusService } from 'src/app/services/ngo-in-focus.service';
@@ -5,7 +6,7 @@ import { Observable } from 'rxjs';
 import { CoffeeConversation } from 'src/app/models/CoffeeConversation';
 import { NgoInFocus } from 'src/app/models/NgoInFocus';
 import { CourseInFocusService } from 'src/app/services/course-in-focus.service';
-import { CourseInFocus } from 'src/app/models/courseInFocus';
+import { CourseInFocus } from 'src/app/models/CourseInFocus';
 import { NewsService } from 'src/app/services/news.service';
 import { News } from 'src/app/models/News';
 
@@ -20,12 +21,16 @@ export class HomePage implements OnInit {
   ngosInFocus$: Observable<NgoInFocus[]>;
   coursesInFocus$: Observable<CourseInFocus[]>;
   newsList$: Observable<News[]>;
+  gridCols = 1;
+  newsColSpan = 1;
+  sidebarColSpan = 1;
 
   constructor(
     private newsService: NewsService,
     private coffeeConversationService: CoffeeConversationService,
     private ngoService: NgoInFocusService,
-    private courseService: CourseInFocusService
+    private courseService: CourseInFocusService,
+    private breakpointObserver: BreakpointObserver
   ) {}
 
   ngOnInit(): void {
@@ -37,6 +42,28 @@ export class HomePage implements OnInit {
     this.coffeeConversations$ = this.coffeeConversationService.getCoffeeConversations();
     this.ngosInFocus$ = this.ngoService.getActiveNgosInFocus();
     this.coursesInFocus$ = this.courseService.getCoursesInFocus();
+
+    this.breakpointObserver
+      .observe([
+        Breakpoints.XSmall,
+        Breakpoints.Small,
+        Breakpoints.Medium,
+        Breakpoints.Large,
+        Breakpoints.XLarge,
+      ])
+      .subscribe((result) => {
+        if (result.breakpoints[Breakpoints.XSmall] || result.breakpoints[Breakpoints.Small]) {
+          this.gridCols = 1;
+          this.newsColSpan = this.sidebarColSpan = 1;
+        } else if (result.breakpoints[Breakpoints.Medium]) {
+          this.gridCols = 2;
+          this.newsColSpan = this.sidebarColSpan = 1;
+        } else {
+          this.gridCols = 12;
+          this.newsColSpan = 8;
+          this.sidebarColSpan = 4;
+        }
+      });
   }
 
   onAccept(): void {
