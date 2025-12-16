44fd11789a2291114cae68f713a08795a063821e
SoujanyaSakinal <soujanyasakinal03@gmail.com>
2025-12-16 21:31:56 +0530

    Removed unused social-share component files

--- src/app/app-common.module.ts
+++ src/app/app-common.module.ts
@@ -9,7 +9,7 @@ import { AqiWidgetComponent } from './components/aqi-widget/aqi-widget.component
 import { BlogCardComponent } from './components/blog-card/blog-card.component';
 import { BlogSliderComponent } from './components/blog-slider/blog-slider.component';
 import { BackButtonComponent } from './components/back-button/back-button.component';
-import { SocialShareComponent } from './components/social-share/social-share.component';
+// import { SocialShareComponent } from './components/social-share/social-share.component';
 import { ElementWelcomeImageComponent } from './components/element-welcome-image/element-welcome-image.component';
 import { EnergyWidgetComponent } from './components/energy-widget/energy-widget.component';
 import { EQWidgetComponent } from './components/eq-widget/eq-widget.component';
@@ -64,7 +64,7 @@ const COMPONENTS = [
   PollResultComponent,
   WiofSpinnerComponent,
   BackButtonComponent,
-  SocialShareComponent,
+  // SocialShareComponent,
   VideoCardComponent,
   BreakingNewsComponent,
   EqWidgetTestComponent,
