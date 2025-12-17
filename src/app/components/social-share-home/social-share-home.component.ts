f039502b5d806ca1d742d0c893464ccc0bfd3130
SoujanyaSakinal <soujanyasakinal03@gmail.com>
2025-12-17 15:19:43 +0530

    Final update- removed unwanted comments

--- src/app/components/social-share-home/social-share-home.component.ts
+++ src/app/components/social-share-home/social-share-home.component.ts
@@ -9,9 +9,7 @@ export class SocialShareHomeComponent implements OnInit {
   title: string = 'Checkout this amazing website Worldisonefamily.com..!!';
   url: string = window.location.href;
   facebookLink: string = `https://www.facebook.com/100071449783619/`;
-  // https://www.linkedin.com/shareArticle?url=[post-url]&title=[post-title]
   linkedinLink: string = `https://www.linkedin.com/company/world-is-one-family?originalSubdomain=in`;
-  // twitterShareLink: string = `https://twitter.com/share?url=${this.url}&text=${this.title}`;
   instagramLink: string = `https://www.instagram.com/wiof_social`;
   whatsappShareLink: string = `https://api.whatsapp.com/send?text=${this.title} ${this.url}`;
 
